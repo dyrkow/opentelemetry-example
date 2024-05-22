173a34037448170d1af228f1f5d0a91a7ee84246
dyrkow <webworkdeveloper@gmail.com>
2024-05-22 21:24:28 +0300

    Optimize base tracing

--- instrumentation.js
+++ instrumentation.js
@@ -1,17 +1,9 @@
-/*instrumentation.js*/
-// Require dependencies
 const { NodeSDK } = require("@opentelemetry/sdk-node");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
 const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
-
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} = require("@opentelemetry/sdk-metrics");
 
 const sdk = new NodeSDK({
   traceExporter: new JaegerExporter({
